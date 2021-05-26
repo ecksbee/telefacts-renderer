18f8356fd772afd7b669f98e2f234b13eede19f6
Mark Wetherington <mark.wetherington@ecksbee.com>
2021-05-25 20:55:11 -0400

    selectize test for rendering the label of a selected option

--- src/components/selectize.test.js
+++ src/components/selectize.test.js
@@ -16,3 +16,18 @@ import './selectizeBox.css';
 
     testArray.forEach(item => expect(screen.getByText(item.label)).toBeTruthy());
   });
+
+  test('render the label of a selected option', () => {
+    const testArray = [
+      {label:'testLabel_1', key: 'testKey_1'},
+      {label:'testLabel_2', key: 'testKey_2'},
+      {label:'testLabel_3', key: 'testKey_3'}
+    ];
+    const testLabel = testArray[0];
+
+    render(<Selectize onChange={_=>{}} selected={testLabel} options={testArray} />);
+
+    expect(()=>screen.getByText('testLabel_1')).not.toThrow();
+    expect(()=>screen.getByText('testLabel_2')).toThrow();
+    expect(()=>screen.getByText('testLabel_3')).toThrow();
+  });
