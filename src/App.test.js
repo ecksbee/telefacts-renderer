b692e3f31af153b3fc9a257a98c5ea90200d3c3b
Mark Wetherington <mark.wetherington@ecksbee.com>
2021-06-08 22:19:17 -0400

    fetch test - in progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import App from './App';
+import renderablesCatalog from './testRenderablesCatalog';
 
-test('renders the app with no errors or failures', () => {  expect(() => {
-  jest.spyOn(URLSearchParams.prototype, 'get').mockImplementation((key) => {
+test('renders the app with no errors or failures', () => {
+  expect(() => {
+    jest.spyOn(URLSearchParams.prototype, 'get').mockImplementation((key) => {
       if (key === 'uuid') {
         return 'abc';
       }
@@ -10,4 +12,23 @@ test('renders the app with no errors or failures', () => {  expect(() => {
     });
     render(<App />)
   }).not.toThrow();
+});
+
+test('renders the app with a given query string of uuid and successfully calls fetch', async () => {
+  const testUuid = 'testUuid';
+  jest.spyOn(URLSearchParams.prototype, 'get').mockImplementation((key) => {
+    if (key === 'uuid') {
+      return testUuid;
+    }
+    return null;
+  });
+  const waitForMe = Promise.resolve({
+    json: () => Promise.resolve(renderablesCatalog),
+  });
+  global.fetch = jest.fn(() =>
+    waitForMe
+  );
+  await act(() => render(<App />));
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith('/projects/' + testUuid + '/renderables');
 });
