63f3ad02d17392f14701ca6e60c6d1b1aaee82bd
Mark Wetherington <mark.wetherington@ecksbee.com>
2021-06-09 21:54:45 -0400

    first dropdown item selected test

--- src/App.test.js
+++ src/App.test.js
@@ -34,4 +34,27 @@ test('renders the app with a given query string of uuid and successfully calls f
   )
   expect(fetchMock).toHaveBeenCalledTimes(1);
   expect(fetchMock).toHaveBeenCalledWith('/projects/' + testUuid + '/renderables');
+  global.fetch.mockRestore();
+});
+
+test('first item in dropdown is selected by default', async () => {
+  const testUuid = 'testUuid';
+  jest.spyOn(URLSearchParams.prototype, 'get').mockImplementation((key) => {
+    if (key === 'uuid') {
+      return testUuid;
+    }
+    return null;
+  });
+  const waitForMe = Promise.resolve({ json: () => Promise.resolve(renderablesCatalog) })
+  jest.spyOn(global, 'fetch')
+    .mockImplementation(() => waitForMe)
+  await act(
+    async () => {
+      render(<App />)
+      await waitForMe
+    }
+  )
+  expect(() => screen.getByText(renderablesCatalog.Subjects[0].Name)).not.toThrow();
+  expect(() => screen.getByText(renderablesCatalog.RelationshipSets[0].Title)).not.toThrow();
+  global.fetch.mockRestore();
 });
