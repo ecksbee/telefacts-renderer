1c8e8b41a0a478af20a99c01a8d9200ef7fd9492
Mark Wetherington <mark.wetherington@ecksbee.com>
2021-06-29 21:07:05 -0400

    added concept network viewer testing

--- src/components/conceptNetworkViewer.test.js
+++ src/components/conceptNetworkViewer.test.js
@@ -1,10 +1,7 @@
 import { act, render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import ConceptNetworkViewer from './conceptNetworkViewer';
 import testRenderables from './testRenderables';
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve,ms));
-}
 
 test('tabs are rendered', () => {
     render(<ConceptNetworkViewer />);
@@ -27,5 +24,40 @@ test('tabs are rendered', () => {
     )
     expect(container.getElementsByClassName('loader').length).toBe(1);
     await waitForElementToBeRemoved(document.querySelector('.loader'));
+    expect(container.getElementsByClassName('loader').length).toBe(0);
     global.fetch.mockRestore();
+  });
+
+  test('correct tab is selected', async () => {
+    const testUuid = 'testUuid';
+    const testHash = 'testHash';
+    const waitForMe = Promise.resolve({ json: () => {Promise.resolve({testRenderables})} })
+    jest.spyOn(global, 'fetch')
+      .mockImplementation(() => waitForMe)
+    act(() => {
+      render(<ConceptNetworkViewer uuidFromQuery={testUuid} renderablesHash={testHash}/>)
+      }
+    )
+    await waitForElementToBeRemoved(document.querySelector('.loader'));
+    expect(screen.getByText("Presentation")).toHaveClass('tab-selected');
+    expect(screen.getByText("Definition")).not.toHaveClass('tab-selected');
+    expect(screen.getByText("Calculation")).not.toHaveClass('tab-selected');
+
+    userEvent.hover(screen.getByText('Definition'));
+    userEvent.click(screen.getByText('Definition'));
+    expect(screen.getByText("Presentation")).not.toHaveClass('tab-selected');
+    expect(screen.getByText("Definition")).toHaveClass('tab-selected');
+    expect(screen.getByText("Calculation")).not.toHaveClass('tab-selected');
+
+    userEvent.hover(screen.getByText('Calculation'));
+    userEvent.click(screen.getByText('Calculation'));
+    expect(screen.getByText("Presentation")).not.toHaveClass('tab-selected');
+    expect(screen.getByText("Definition")).not.toHaveClass('tab-selected');
+    expect(screen.getByText("Calculation")).toHaveClass('tab-selected');
+
+    userEvent.hover(screen.getByText('Presentation'));
+    userEvent.click(screen.getByText('Presentation'));
+    expect(screen.getByText("Presentation")).toHaveClass('tab-selected');
+    expect(screen.getByText("Definition")).not.toHaveClass('tab-selected');
+    expect(screen.getByText("Calculation")).not.toHaveClass('tab-selected');
   });
