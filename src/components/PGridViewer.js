56218330a169345380e25f2be080ba28c3c015bd
Mark Wetherington <mark.wetherington@ecksbee.com>
2021-07-12 22:08:12 -0400

    everything but factual quadrant looking good

--- src/components/PGridViewer.js
+++ src/components/PGridViewer.js
@@ -4,52 +4,92 @@ import ReactDataSheet from 'react-datasheet';
 import 'react-datasheet/lib/react-datasheet.css';
 
 class PGridViewer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {grid:null};
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: null
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!this.props.renderablesData) {
+      return
+    }
+    if (prevProps && prevProps.renderablesData && this.props.renderablesData && (prevProps.renderablesData===this.props.renderablesData)) {
+      return
     }
 
-    componentDidUpdate(prevProps) {
-      if (!this.props.renderablesData || (prevProps && prevProps.renderablesData && this.props.renderablesData)) {
-        return
-      }
-      
-      const maxColumns = this.props.renderablesData.PGrid.MaxIndentation+1+this.props.renderablesData.PGrid.FactualQuadrant[0].length;
-      const maxRows = this.props.renderablesData.PGrid.MaxDepth+1+this.props.renderablesData.PGrid.FactualQuadrant.length;
+    const PGrid = this.props.renderablesData.PGrid;
 
-      let grid = [];
-  
-      for (let i=0; i<maxRows; i++) {
-        grid.push([]);
-        for (let j=0; j<maxColumns; j++) {
-            grid[i].push({value: i + ' x ' + j});
+    const maxColumns = PGrid.MaxIndentation + PGrid.FactualQuadrant[0].length;
+    const maxRows = PGrid.MaxDepth + 1 + PGrid.FactualQuadrant.length;
+    
+    let grid = [];
+
+    for (let i = 0; i < maxRows; i++) {
+      grid.push([]);
+      for (let j = 0; j < maxColumns; j++) {
+        let cellValue = '';
+        if (i === 0 && j >= PGrid.MaxIndentation) {
+          cellValue = PGrid.RelevantContexts[j - PGrid.MaxIndentation].PeriodHeader.Unlabelled;
+        } else if (i <= PGrid.MaxDepth && j >= PGrid.MaxIndentation) {
+          if (PGrid.RelevantContexts[j-PGrid.MaxIndentation].Dimensions.length>0) {
+            cellValue = PGrid.RelevantContexts[j-PGrid.MaxIndentation].Dimensions[0].ExplicitMember.Label.Default["en - english"];
           }
-      }
-      this.setState({grid});
-  }
-
-    render() {
-        if (!this.state.grid) {
-            return null;
+        } else if (i > PGrid.MaxDepth) {
+          if (j < PGrid.MaxIndentation) {
+            if (PGrid.IndentedLabels[i-PGrid.MaxDepth-1].Indentation === j) {
+              cellValue = PGrid.IndentedLabels[i-PGrid.MaxDepth-1].Label.Default.Unlabelled;
+            }
+          }
         }
-      return (
-        <ReactDataSheet
-          data={this.state.grid}
-          valueRenderer={cell => cell.value}
-          onCellsChanged={changes => {
-            const grid = this.state.grid.map(row => [...row]);
-            changes.forEach(({ cell, row, col, value }) => {
-              grid[row][col] = { ...grid[row][col], value };
-            });
-            this.setState({ grid });
-          }}
-        />
-      );
+        grid[i].push({
+          value: cellValue
+        });
+      }
     }
+    this.setState({
+      grid
+    });
   }
 
-  PGridViewer.propTypes = {
-    renderablesData: PropTypes.object
+  render() {
+    if (!this.state.grid) {
+      return null;
+    }
+    return ( <
+      ReactDataSheet data = {
+        this.state.grid
+      }
+      valueRenderer = {
+        cell => cell.value
+      }
+      onCellsChanged = {
+        changes => {
+          const grid = this.state.grid.map(row => [...row]);
+          changes.forEach(({
+            cell,
+            row,
+            col,
+            value
+          }) => {
+            grid[row][col] = {
+              ...grid[row][col],
+              value
+            };
+          });
+          this.setState({
+            grid
+          });
+        }
+      }
+      />
+    );
+  }
+}
+
+PGridViewer.propTypes = {
+  renderablesData: PropTypes.object
 };
 
-  export default PGridViewer
+export default PGridViewer
