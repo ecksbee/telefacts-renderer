865dc2e5cef1c9dd3481e38ce3b0aad4fd814fc6
Joshua Nario <joshua.nario@hotmail.com>
2021-08-18 00:18:08 -0400

    arc diagram css

--- src/components/rViewer/conceptNetworkViewer/definition/arcDiagram.js
+++ src/components/rViewer/conceptNetworkViewer/definition/arcDiagram.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
+import './arcDiagram.css'
  
 const ArcDiagram = ({ data }) => {
     const svgRef = React.useRef(null);
@@ -7,13 +8,25 @@ const ArcDiagram = ({ data }) => {
         if (!data.nodes.length || !data.links.length) {
             return
         }
-        const 
-            width = 1300,
-            height = 800;
+        const charWidth = 14;
+        let maxChar = 0
+        data.nodes.forEach(
+            node => {
+                if (node.name.length > maxChar) {
+                    maxChar = node.name.length
+                }
+            }
+        )
+        const magic = maxChar * charWidth,
+            dMagic = data.nodes.length * charWidth,
+            xMagic = Math.sin(Math.PI/4)*magic,
+            yMagic = Math.cos(Math.PI/4)*magic,
+            width = dMagic + 2*xMagic,
+            height = yMagic + dMagic*0.5;
 
         const svg = d3.select(svgRef.current)
             .append("svg")
-            .attr("viewBox",[0,0,width+90,height+80])
+            .attr("viewBox",[0,0,width,height])
             .attr("width", width)
             .attr("height", height)
             .append("g");
@@ -30,7 +43,7 @@ const ArcDiagram = ({ data }) => {
             .range([0.5,8]);
     
         const x = d3.scalePoint()
-            .range([0, width])
+            .range([0, 2*dMagic])
             .domain(allNodes)
     
         const idToNode = {};
@@ -43,8 +56,8 @@ const ArcDiagram = ({ data }) => {
             .data(data.links)
             .join('path')
             .attr('d', d => {
-                let start = x(idToNode[d.source].name)    // X position of start node on the X axis
-                let end = x(idToNode[d.target].name)      // X position of end node
+                let start = x(idToNode[d.source].name) + xMagic    // X position of start node on the X axis
+                let end = x(idToNode[d.target].name) + xMagic      // X position of end node
                 return ['M', start, height/2-18,    // the arc starts at the coordinate x=start, y=height-30 (where the starting node is)
                 'A',                            // This means we're gonna build an elliptical arc
                 (start - end)/2, ',',    // Next 2 lines are the coordinates of the inflexion point. Height of this point is proportional with start - end distance
@@ -57,28 +70,36 @@ const ArcDiagram = ({ data }) => {
             .style("stroke-width", 1)
     
         svg.selectAll("mynodes")
-            .data(data.nodes.sort((a,b) => b.n - a.n ))
+            .data(data.nodes.sort((a,b) =>{
+                if (a.name < b.name) {
+                    return -1;
+                }
+                if (a.name > b.name) {
+                    return 1;
+                }
+                return 0;
+            } ))
             .join("circle")
-                .attr("cx", d=>x(d.name))
-                .attr("cy", height-30)
-                .attr("r", d=>size(d.n))
-                .style("fill", d=> color(d.grp))
-                .attr("stroke", "white")
+            .attr("cx", d=>x(d.name) + xMagic)
+            .attr("cy", height-30)
+            .attr("r", d=>size(d.n))
+            .style("fill", d=> color(d.grp))
+            .attr("stroke", "white")
     
         svg.selectAll("mylabels")
             .data(data.nodes)
             .join("text")
-                .attr("x", 0)
-                .attr("y", 0)
-                .text(d=>d.name)
-                .style("text-anchor", "end")
-                .attr("transform",d=>`translate(${x(d.name)},${height/2-15}) rotate(-45)`)
-                .style("font-size", 18)
-                .style("font-family", "Calibri")
+            .attr("x", 0)
+            .attr("y", 0)
+            .text(d=>d.name)
+            .style("text-anchor", "end")
+            .attr("transform",d=>`translate(${x(d.name) + xMagic},${height/2-15}) rotate(-45)`)
+            .style("font-size", 18)
+            .style("font-family", "Calibri")
     
     }, [data]);
  
-  return <div ref={svgRef} />;
+  return <div id='arc-diagram' ref={svgRef} />;
 };
 
 export default ArcDiagram;
