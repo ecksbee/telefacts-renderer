05ed575be13bf91ea73afe64aa4f1c0bc61d2590
Joshua Nario <joshua.nario@hotmail.com>
2021-08-19 01:00:25 -0400

    rootDomainViewer

--- src/components/rViewer/conceptNetworkViewer/definition/rootDomainViewer.js
+++ src/components/rViewer/conceptNetworkViewer/definition/rootDomainViewer.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FactViewer from './facts';
+import DGridDomains from './domains';
 
 function RootDomainViewer({rootDomain}) {
     const [isViewingFacts, setFactViewer] = React.useState(true);
-    const [effectiveDimension, setEffectiveDimension] = React.useState(rootDomain.EffectiveDimensions[0]);
     const controls = <div>
         <div>
             <button className={isViewingFacts? "tab-selected stacked" : "stacked"} onClick={()=>setFactViewer(true)}>Facts</button>
             <button className={!isViewingFacts? "tab-selected stacked" : "stacked"} onClick={()=>setFactViewer(false)}>Domains</button>
         </div>
-        {!isViewingFacts && 
-            <select>
-                {
-                    rootDomain.EffectiveDimensions.map(
-                        dim => <option key={dim.Href} selected={dim.Href===effectiveDimension.Href} onSelect={()=>setEffectiveDimension(dim)}>{dim.Label.Default.Unlabelled}</option>
-                    )
-                }
-            </select>
-        }
     </div>
 
     return (
       <div>
           {controls}
           {isViewingFacts && <FactViewer rootDomain={rootDomain} />}
+          {!isViewingFacts && <DGridDomains rootDomain={rootDomain} />}
       </div>
     );
 }
